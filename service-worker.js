a47d6fe40dd15ddcaf5817427bb5986bcc1db09f
takuyaameku <takuya.a.0124@gmail.com>
2025-12-07 15:41:45 +0900

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+// service-worker.js (ファイルの内容)
+// このファイルはPWAのオフライン機能に必要ですが、
+// まずは空にしておき、後でキャッシュ戦略を記述します。
+// 例:
+// self.addEventListener('install', (e) => {
+//   e.waitUntil(
+//     caches.open('app-cache-v1').then((cache) => {
+//       return cache.addAll(['/認知症予防アプリ.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png']);
+//     })
+//   );
+// });
+// ...

38168f93ef71a65da60289a5107eb8bfe2826918
takuyaameku <takuya.a.0124@gmail.com>
2026-05-07 17:02:03 +0900

    Complete PWA setup, update sitemap for reha-activelab.com, and add all professional content

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,11 @@
-// service-worker.js (ファイルの内容)
-// このファイルはPWAのオフライン機能に必要ですが、
-// まずは空にしておき、後でキャッシュ戦略を記述します。
-// 例:
-// self.addEventListener('install', (e) => {
-//   e.waitUntil(
-//     caches.open('app-cache-v1').then((cache) => {
-//       return cache.addAll(['/認知症予防アプリ.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png']);
-//     })
-//   );
-// });
-// ...
+// service-worker.js
+// PWA対応のためのService Worker
+// オフライン機能や表示速度向上のために使用します。
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+});
